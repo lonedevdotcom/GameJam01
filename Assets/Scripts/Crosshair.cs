4c0f5437e9b888550d34e80a9f91b6c22e878fe2
Richard Hawkes <richard_hawkes@yahoo.co.uk>
2016-07-13 21:42:38 +0100

    Check-in after first session.

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	public Texture2D crosshairTexture; 
+	public Rect position; 
+	static bool OriginalOn = true;
+
+	void Start() 
+	{
+		position = new Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - crosshairTexture.height) /2, crosshairTexture.width, crosshairTexture.height);
+	}
+
+	void OnGUI() 
+	{
+		//Screen.lockCursor = true; 
+		// Debug.Log(Cursor.lockState);
+		// Cursor.lockState = CursorLockMode.Locked;
+		// Cursor.visible = false;
+		// Screen.showCursor = false; 
+
+		if(OriginalOn == true) 
+			GUI.DrawTexture(position, crosshairTexture); 
+	}
+}
